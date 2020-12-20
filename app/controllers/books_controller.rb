c894ca27dd02b81cedbefb7598dd91501cce3cea
shiro1202 <black0157@icloud.com>
2020-12-20 08:32:37 +0000

    Complete

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,52 @@
+class BooksController < ApplicationController
+  def top
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+      if @book.save
+        flash[:notice] = "Book was successfully created"
+        redirect_to book_path(@book)
+      else
+        flash.now[:error] = "error",@book.errors.full_messages
+        @books = Book.all
+        render :index
+      end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+      if @book.update(book_params)
+        flash[:notice] = "Book was successfully updated"
+        redirect_to book_path(params[:id])
+      else
+        flash.now[:error] = "error",@book.errors.full_messages
+        render :edit
+      end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = "Book was successfully destroyed"
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
